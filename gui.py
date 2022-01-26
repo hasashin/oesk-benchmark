40d20026b94bc6356c7b66279d99cf10c7080d62
hasashin <dominik.hazak@gmail.com>
2022-01-26 08:17:51 +0100

    final

---
+++ gui.py
@@ -0,0 +1,219 @@
+from curses import window
+import tkinter as tk
+import tkinter.ttk as ttk
+import threading
+from turtle import left
+import matplotlib.pyplot as plt
+
+class Plotter:
+
+    def __init__(self, table):
+        self.data = table
+
+    def plot_results(self, type):
+        x = [int(x_val) for x_val in list(self.data)]
+        y = [self.data['{}'.format(i)].microseconds / 1000000.0 for i in list(self.data)]
+        plt.scatter(x, y, marker='.')
+        plt.xlabel('Liczba rekordów w każdej z tabel')
+        plt.ylabel('Czas wykonania zapytania')
+        plt.title('Wykres wydajności bazy {}'.format(type))
+        plt.grid(True)
+        plt.show()
+
+class DbGui:
+    db_type = {"MySQL":1, "PostgreSQL":2, "SQLite":3}
+
+    def __init__(self, db_connector):
+        self.db_connector = db_connector
+
+    results = None
+    window = None
+    vars={'selected_db':None, 'server_address':None, 'progress':None}
+    times={}
+
+    def show_results(self):
+        pltr = Plotter(self.times)
+        pltr.plot_results([k for k,v in self.db_type.items() if v == self.db_connector.type][0])
+
+    def start_benchmark(self):
+        self.vars['progress']['value'] = 0
+        try:
+            self.db_connector.start(0)
+        except AttributeError:
+            try:
+                self.db_connector.connect_to_database(self.vars['server_address'])
+            except:
+                tk.Message(window, text='Failed to connect to database. Please check your address and try again.')
+                return
+        threading.Thread(target=self.start_benchmark_thread).start()
+
+    def start_benchmark_thread(self):
+        self.vars['progress_label'].set('Running test 1 of 10')
+        self.times['100'] = self.db_connector.start(100)
+        self.vars['progress']['value'] += 10
+
+        self.vars['progress_label'].set('Running test 2 of 10')
+        self.times['200'] = self.db_connector.start(200)
+        self.vars['progress']['value'] += 10
+
+        self.vars['progress_label'].set('Running test 3 of 10')
+        self.times['300'] = self.db_connector.start(300)
+        self.vars['progress']['value'] += 10
+        
+        self.vars['progress_label'].set('Running test 4 of 10')
+        self.times['400'] = self.db_connector.start(400)
+        self.vars['progress']['value'] += 10
+
+        self.vars['progress_label'].set('Running test 5 of 10')
+        self.times['500'] = self.db_connector.start(500)
+        self.vars['progress']['value'] += 10
+
+        # self.vars['progress_label'].set('Running test 6 of 10')
+        # self.times['600'] = self.db_connector.start(600)
+        # self.vars['progress']['value'] += 10
+
+        # self.vars['progress_label'].set('Running test 7 of 10')
+        # self.times['700'] = self.db_connector.start(700)
+        # self.vars['progress']['value'] += 10
+        
+        # self.vars['progress_label'].set('Running test 8 of 10')
+        # self.times['800'] = self.db_connector.start(800)
+        # self.vars['progress']['value'] += 10
+
+        # self.vars['progress_label'].set('Running test 9 of 10')
+        # self.times['900'] = self.db_connector.start(900)
+        # self.vars['progress']['value'] += 10
+
+        # self.vars['progress_label'].set('Running test 10 of 10')
+        # self.times['1000'] = self.db_connector.start(1000)
+        # self.vars['progress']['value'] += 10
+        self.vars['progress']['value'] = 100
+
+        self.vars['progress_label'].set('Tests completed!')
+
+    def set_grid_responsive(self, root:tk.Tk):
+        n_rows = 5
+        n_columns = 3
+        for i in range(n_rows):
+            root.grid_rowconfigure(i,  weight =1)
+        for i in range(n_columns):
+            root.grid_columnconfigure(i,  weight =1)
+
+    def update_db(self, event):
+        self.db_connector.type = self.db_type[self.vars['selected_db'].get()]
+        self.db_connector.connect_to_database(self.vars['server_address'])
+
+    def prepare_widgets(self, root:tk.Tk):
+        title = tk.Label(\
+            root, \
+            text='This app is benchmarking database engine of your choice'\
+        )
+        title.pack()
+
+        frame = tk.Frame(root, borderwidth=2)
+        frame.pack(side='top', pady=20)
+
+        step_1 = tk.Label(\
+            frame, \
+            text='1. Select database engine'\
+        )
+        step_1.pack()
+        
+        self.vars['selected_db'] = tk.StringVar()
+        combo_engine = ttk.Combobox(\
+            frame, \
+            values=list(self.db_type), \
+            textvariable=self.vars['selected_db'] \
+        )
+        combo_engine.current(0)
+        combo_engine.pack( \
+            anchor='w', \
+            padx=20, \
+            pady=10 \
+        )
+        combo_engine.bind('<<ComboboxSelected>>', self.update_db)
+
+        step_2 = tk.Label(\
+            frame, \
+            text='2. Type server url'\
+        )
+        step_2.pack()
+
+        self.vars['server_address'] = tk.StringVar()
+        entry_server = tk.Entry( \
+            frame, \
+            width=20, \
+            textvariable=self.vars['server_address'] \
+        )
+        entry_server.pack( \
+            pady=10 \
+        )
+
+        bottom_frame = tk.Frame(root)
+        bottom_frame.pack(side='left', anchor='sw')
+
+        self.vars['progress'] = ttk.Progressbar(bottom_frame, length = 280)
+        self.vars['progress'].pack( \
+            side='left', \
+            anchor='sw', \
+            padx=10, \
+            pady=10 \
+        )
+
+        self.vars['progress_label'] = tk.StringVar()
+        prog_label = tk.Label(\
+            bottom_frame,\
+            textvariable=self.vars['progress_label']
+        )
+        prog_label.pack(\
+            before=self.vars['progress'], \
+            anchor='s' \
+        )
+
+        btn = tk.Button(\
+            root, \
+            text="Start", \
+            command=self.start_benchmark \
+        )
+        btn.pack( \
+            side='right', \
+            anchor='se', \
+            padx=5, \
+            pady=5 \
+        )
+
+        btn2 = tk.Button( \
+            root, \
+            text="Show results", \
+            command=self.show_results \
+        )
+        btn2.pack( \
+            before=btn, \
+            side='right', \
+            anchor='se', \
+            padx=5, \
+            pady=5 \
+        )
+
+    def prepare_window(self):
+        root = tk.Tk()
+        root.title('DataBase benchmark')
+        root.geometry("500x400")
+        root.minsize(width=500, height=400)
+        return root
+    
+    def prepare(self):
+        self.window = self.prepare_window()
+        self.prepare_widgets(self.window)
+
+    def update_gui(self):
+        self.window.update()
+        self.window.after(3000, self.update_gui)
+
+    def start(self):
+        self.window.mainloop()
+        self.update_gui()
+
+if __name__== '__main__':
+    print('This script is not meant to be run')
+    exit(1)
---
+++ main.py
@@ -0,0 +1,148 @@
+#!python3
+from datetime import datetime
+import email
+from statistics import median
+from tkinter import StringVar
+from peewee import *
+from playhouse.db_url import connect
+from gui import DbGui
+from faker import Faker
+from argon2 import PasswordHasher
+
+db = DatabaseProxy()
+
+class DBConnector:
+    def __init__(self):
+        self.type = DbGui.db_type["MySQL"]
+
+    def connect_to_database(self, url:StringVar):
+        global db
+        db
+        if self.type == DbGui.db_type["MySQL"]:
+            if url.get().startswith('mysql://'):
+                db.initialize(connect(url.get()))
+            else:
+                url.set('mysql://')
+        elif self.type == DbGui.db_type['PostgreSQL']:
+            if url.get().startswith('postgresql://'):
+                db.initialize(connect(url.get()))
+            else:
+                url.set('postgresql://')
+        elif self.type == DbGui.db_type['SQLite']:
+            if url.get().startswith('sqlite:///'):
+                db.initialize(connect(url.get()))
+            else:
+                url.set('sqlite:///')
+        else:
+            raise ValueError('Invalid database type')
+
+    def start(self, num:int):
+        db.connect()
+
+        Comment.bind(db)
+        Content.bind(db)
+        Media.bind(db)
+        Account.bind(db)
+
+        db.drop_tables([Comment,Content,Media,Account])
+        db.create_tables([Comment,Content,Media,Account])
+        # prepare data
+        fake_data={'Account':[], 'Content':[], 'Comment':[], 'Media': []}
+        fake = Faker()
+        ph = PasswordHasher()
+        for _ in range(num):
+            account = {}
+            account['email'] = fake.company_email()
+            account['password'] = ph.hash(fake.password(length=16))
+            account['name'] = fake.name()
+            account['token'] = fake.uuid4()
+            account['isAdmin'] = fake.pybool()
+            created_date = datetime.strptime(fake.date(), '%Y-%m-%d')
+            account['dateCreated'] = created_date
+            account['lastLogin'] = fake.date_between(start_date=created_date)
+            fake_data['Account'].append(account)
+
+            content = {}
+            content['isPage'] = fake.pybool()
+            content['isHidden'] = fake.pybool()
+            content['slug'] = fake.name()
+            content['title'] = fake.word()
+            content['body'] = fake.paragraph(nb_sentences=10)
+            created_date = datetime.strptime(fake.date(), '%Y-%m-%d')
+            content['dateCreated'] = created_date
+            content['dateModified'] = fake.date_between(start_date=created_date)
+            content['authorId'] = fake.pyint(min_value=0, max_value=num-1)
+            content['modifiedById'] = fake.pyint(min_value=0, max_value=num-1)
+            fake_data['Content'].append(content)
+
+            comment = {}
+            comment['body'] = fake.paragraph(nb_sentences=3)
+            comment['dateCreated'] = datetime.strptime(fake.date(), '%Y-%m-%d')
+            comment['authorId'] = fake.pyint(min_value=0, max_value=num-1)
+            comment['contentId'] = fake.pyint(min_value=0, max_value=num-1)
+            fake_data['Comment'].append(comment)
+
+            media = {}
+            media['name'] = fake.file_name(category='image')
+            media['path'] = fake.file_path()
+            media['dateCreated'] = datetime.strptime(fake.date(), '%Y-%m-%d')
+            media['authorId'] = fake.pyint(min_value=0, max_value=num-1)
+            fake_data['Media'].append(media)
+        
+        starttime = datetime.now()
+        for type in fake_data.keys():
+                if type == 'Account':
+                    for row in db.batch_commit(fake_data[type], num):
+                        Account.create(**row)
+                elif type == 'Content':
+                    for row in db.batch_commit(fake_data[type], num):
+                        Content.create(**row)
+                elif type == 'Comment':
+                    for row in db.batch_commit(fake_data[type], num):
+                        Comment.create(**row)
+                elif type == 'Media':
+                    for row in db.batch_commit(fake_data[type], num):
+                        Media.create(**row)
+                else:
+                    raise ValueError("Type of data is invalid")
+        stoptime = datetime.now()
+        db.close()
+        return stoptime-starttime
+
+
+class Account(Model):
+    email = CharField()
+    password = CharField()
+    name = CharField()
+    token = CharField()
+    isAdmin = BooleanField(default=False)
+    dateCreated = DateField()
+    lastLogin = DateField()
+
+class Content(Model):
+    isPage = BooleanField(default=False)
+    isHidden = BooleanField(default=False)
+    slug = CharField()
+    title = CharField()
+    body = TextField()
+    dateCreated = DateField()
+    dateModified = DateField()
+    authorId = ForeignKeyField(Account)
+    modifiedById = ForeignKeyField(Account)
+
+class Comment(Model):
+    body = CharField()
+    dateCreated = DateField()
+    authorId: ForeignKeyField(Account)
+    contentId: ForeignKeyField(Content)
+
+class Media(Model):
+    name = CharField()
+    path = CharField(default='')
+    dateCreated = DateField()
+    authorId = ForeignKeyField(Account)
+
+if __name__ == '__main__':
+    gui = DbGui(DBConnector())
+    gui.prepare()
+    gui.start()
